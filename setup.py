4143f6ee842692666ff964e95095cef63d1f82d7
muhammadsayuti <muhammad.sayuti94@gmail.com>
2019-11-10 13:19:25 +0800

    allow install using setuptools

--- setup.py
+++ setup.py
@@ -1,14 +1,54 @@
-# Always prefer setuptools over distutils
+import sys
 import re
 import os
 import ast
-from setuptools import setup
+import atexit
+import os.path as path
+from setuptools import setup, find_packages
 from scripts import setupcommon as common
+from scripts.setupcommon import hasWriteAccess, copyTree, copyFile
+from subprocess import call
 
 try:
     BASEDIR = os.path.dirname(os.path.realpath(__file__))
 except NameError:
     BASEDIR = None
+
+
+def _post_install():
+    if hasWriteAccess('/usr/share'):
+        datadir = '/usr/share'
+        libdir = '/usr/lib'
+        os.makedirs(path.join(datadir, 'luminos'), 0o755, exist_ok=True)
+        os.makedirs(path.join(libdir, 'luminos'), 0o755, exist_ok=True)
+    else:
+        datadir = path.expanduser('~/.local/share')
+        libdir = path.expanduser('~/.local/lib')
+        os.makedirs(path.join(datadir, 'luminos'), 0o755, exist_ok=True)
+        os.makedirs(path.join(libdir, 'luminos'), 0o755, exist_ok=True)
+
+    print('Installing vendor files...')
+    copyTree('data/vendor', path.join(datadir, 'luminos', 'vendor'))
+    print('Installing static templates...')
+    copyTree('data/static', path.join(datadir, 'luminos', 'static'))
+    print('Installing built-in plugins...')
+    copyTree('plugins', path.join(libdir, 'luminos', 'plugins'))
+    print('Installing GSettings schemas...')
+    copyFile('data/io.gitlab.fisma.luminos.gschema.xml', path.join(datadir, 'glib-2.0', 'schemas', 'io.gitlab.fisma.luminos.gschema.xml'))
+
+    print("Updating icon cache...")
+    call(["gtk-update-icon-cache", "-qtf", path.join(datadir, "icons", "hicolor")])
+
+    print("Updating desktop database...")
+    call(["update-desktop-database", "-q", path.join(datadir, "applications")])
+
+    print("Compiling GSettings schemas...")
+    call(["glib-compile-schemas", path.join(datadir, "glib-2.0", "schemas")])
+
+
+args = sys.argv[1:]
+if len(args) > 0 and args[0] != 'sdist':
+    atexit.register(_post_install)
 
 
 def read_file(name):
@@ -57,20 +97,13 @@ with open(os.path.join(BASEDIR, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="luminos",
-    version="0.3.2",
-    packages=[
-        "luminos",
-        "luminos.core",
-        "luminos.widgets",
-        "luminos.windows",
-        "luminos.utils",
-        "luminos.plugins",
-        "luminos.browser",
-    ],
+    version="0.3.6",
+    python_requires='>=3.5',
+    packages=find_packages(exclude=['scripts', 'scripts.*', 'tests', 'tests.*']),
     url="https://gitlab.com/fisma/luminos",
     license="MIT",
     author="Fisma Linux Project",
-    author_email="",
+    author_email="muhammad.sayuti94@gmail.com",
     description="Desktop application SDK for creating Universal Linux Applications.",
     long_description=long_description,
     long_description_content_type='text/markdown',  # Optional (see note above)
@@ -79,17 +112,19 @@ setup(
     #     'dev': ['check-manifest'],
     #     'test': ['coverage'],
     # },
+    entry_points={'gui_scripts':
+                  ['luminos = luminos.luminos:main']},
     package_data={"": ["luminos.yml"]},
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Environment :: X11 Applications",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License (MIT)",
         "Operating System :: POSIX :: Linux",
         "Programming Language :: JavaScript",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Topic :: Software Development",
         "Topic :: Software Development :: Libraries :: Application Frameworks",
         "Topic :: Software Development :: Libraries :: Python Modules",
